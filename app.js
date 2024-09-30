306d454270f3d1495511f03e56d2ab1067b5a948
LLOYDY05 <johnlloyddonque18@gmail.com>
2024-10-01 00:35:00 +0800

    Added Loops

--- app.js
+++ app.js
@@ -27,3 +27,11 @@ const isPrime = (num) => {
     }
     return "Prime";
 };
+// 4. Loops - Check for palindrome
+const isPalindrome = (str) => {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return str === reversed ? "Palindrome" : "Not a palindrome";
+};
