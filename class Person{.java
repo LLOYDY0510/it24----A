3163e2be6dbb2f1e126276121df23c2ebaa7a108
LLOYDY0510 <johnlloyddonque18@gmail.com>
2024-10-11 10:16:59 +0800

    added persomn

---
+++ class Person{.java
@@ -0,0 +1,29 @@
+class Person{
+    constructor(name){
+         this.name = name;
+     } 
+
+     sayName(){
+         console.log(this.name);
+       }
+}
+
+class Student extends Person{
+   constructor(name, rollNumber){
+      super(name);
+      this.rollNumber = rollNumber;
+  }
+
+      logDetails(){
+      console.log(`Name: ${this.name}, Roll
+      number: ${this.rollNumber}`)
+      }
+      
+      sayName(){
+      console.log("From Student");
+      super.sayName();
+      }
+}
+      var student = new Student("Ken jay", 1);
+      student.logDetails();
+      student.sayName();
